1542b99d5af30d736178ca58ab0d71c3547ed7e3
Ramesh Chincholi <ramesh.chincholi@komli.com>
2012-07-18 00:18:23 +0530

    first ibatis call

--- services/src/main/java/rest/com/rest/day/info/HelloWorldService.java
+++ services/src/main/java/rest/com/rest/day/info/HelloWorldService.java
@@ -1,10 +1,21 @@
 package com.rest.day.info;
  
+import java.io.IOException;
+import java.io.Reader;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
- 
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import com.lpdb.mybatis.geo.entity.GeoCities;
+import com.lpdb.mybatis.geo.mapper.GeoCitiesMapper;
+
 @Path("/hello")
 public class HelloWorldService {
  
@@ -14,6 +25,29 @@ public class HelloWorldService {
  
 		String output = "Jersey say : " + msg;
  
+		String resource = "configuration.xml";
+		Reader reader = null;
+		SqlSessionFactory sqlMapper = null;
+		try {
+			reader = Resources.getResourceAsReader(resource);
+			sqlMapper = new SqlSessionFactoryBuilder().build(reader);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		SqlSession session = sqlMapper.openSession();
+		try {
+			GeoCitiesMapper mapper = session.getMapper(GeoCitiesMapper.class);
+			GeoCities geoCities = mapper.selectByPrimaryKey(1);
+			System.out.println(geoCities.getName() + ":"
+						+ geoCities.getDescription());
+			output = geoCities.getName();
+		} finally {
+			session.close();
+		}
+		
+		
 		return Response.status(200).entity(output).build();
  
 	}
